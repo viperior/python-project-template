a7e078d2498f570a4f861923174b771ac9f9f1a8
viperior <viperior@gmail.com>
2021-11-28 16:47:18 -0800

    Add pytest case that automatically lints the project using pylint

---
+++ tests/test_python_code_quality.py
@@ -0,0 +1,20 @@
+"""Test the project's code quality using pylint"""
+
+import logging
+
+from pylint import epylint as lint
+
+
+def test_python_code_quality() -> None:
+    """Run the project's source code through pylint and assert 0 errors should exist"""
+    (pylint_stdout, pylint_stderr) = lint.py_run(command_options="../python-project-template", return_std=True)
+    standard_output_data = pylint_stdout.read()
+    standard_error_data = pylint_stderr.read()
+    logging.debug("pylint_stderr = \n%s", standard_error_data)
+    pylint_error_free_search_phrase = "Your code has been rated at 10/10"
+    pylint_errors_detected = pylint_error_free_search_phrase not in standard_output_data
+
+    if pylint_errors_detected:
+        logging.error("Pylint errors detected:\n%s", standard_output_data)
+
+    assert pylint_error_free_search_phrase in standard_output_data
