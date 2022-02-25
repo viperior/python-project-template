5ea1c6500f5e7b4a3dc135a9dbef6185ba6002b7
viperior <viperior@gmail.com>
2022-02-25 15:45:35 -0800

    Improve coin flip test design
    Increase code coverage of flip_coins by parametrizing the test_flip_coins_function and including test input data that has a higher probability to activate all logical branches

--- tests/test_flip_coins_function.py
+++ tests/test_flip_coins_function.py
@@ -2,16 +2,26 @@
 
 import logging
 
+import pytest
+
 from games_of_chance.flip_coins import flip_coins
 
 
-def test_foo_function() -> None:
+@pytest.mark.parametrize("coin_count", [0, 1, 2, 3, 4, 10, 20])
+def test_foo_function(coin_count: int) -> None:
     """Example test case
+
+    Parameters:
+    coin_count (int): The number of coins to flip
 
     Returns:
     None
     """
-    coin_flip_result = flip_coins(coins_to_flip=10, chosen_side="heads")
-    logging.debug("coin_flip_result = %s", coin_flip_result)
+    try:
+        coin_flip_result = flip_coins(coins_to_flip=coin_count, chosen_side="heads")
+        logging.debug("coin_flip_result = %s", coin_flip_result)
 
-    assert coin_flip_result["result"] in ["won", "lost", "tie"]
+        assert coin_flip_result["result"] in ["won", "lost", "tie"]
+    except AssertionError as error:
+        # Validate that trying to flip 0 coins causes an error
+        assert isinstance(error, AssertionError) and coin_count == 0
