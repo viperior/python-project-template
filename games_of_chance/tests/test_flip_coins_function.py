29b81444ad53560a5efd4fe6ca776f8021dcda3f
Johnathon Stone <viperior@gmail.com>
2022-06-30 16:31:09 +0000

    Improve code readability

--- games_of_chance/tests/test_flip_coins_function.py
+++ games_of_chance/tests/test_flip_coins_function.py
@@ -7,7 +7,10 @@ import pytest
 import games_of_chance.flip_coins
 
 
-@pytest.mark.parametrize("coin_count", list(range(11)) + list(range(10, 200, 10)) + [1, 2] * 30)
+@pytest.mark.parametrize(
+    "coin_count",
+    list(range(11)) + list(range(10, 200, 10)) + [1, 2] * 30
+)
 def test_foo_function(coin_count: int) -> None:
     """Example test case
 
@@ -18,8 +21,10 @@ def test_foo_function(coin_count: int) -> None:
     None
     """
     try:
-        coin_flip_result = games_of_chance.flip_coins.flip_coins(coins_to_flip=coin_count,
-                                                                 chosen_side="heads")
+        coin_flip_result = games_of_chance.flip_coins.flip_coins(
+            coins_to_flip=coin_count,
+            chosen_side="heads"
+        )
         logging.debug("coin_flip_result = %s", coin_flip_result)
 
         assert coin_flip_result["result"] in ["won", "lost", "tie"]
