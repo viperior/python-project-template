6345172b7ec69f24c40133d5f280acab57e7dfd3
viperior <viperior@gmail.com>
2023-03-04 13:44:17 -0800

    Update multi-flip test params and return types

--- src/tests/test_flip_coins_function.py
+++ src/tests/test_flip_coins_function.py
@@ -8,26 +8,25 @@ import src.flip_coins
 
 
 @pytest.mark.parametrize(
-    "coin_count",
+    "coin_flip_count",
     list(range(11)) + list(range(10, 200, 10)) + [1, 2] * 30
 )
-def test_foo_function(coin_count: int) -> None:
+def test_foo_function(coin_flip_count: int) -> None:
     """Example test case
 
     Parameters:
-    coin_count (int): The number of coins to flip
+    coin_flip_count (int): The number of coins to flip
 
     Returns:
     None
     """
     try:
         coin_flip_result = src.flip_coins.flip_coins(
-            coins_to_flip=coin_count,
+            coin_flip_count=coin_flip_count,
             chosen_side="heads"
         )
         logging.debug("coin_flip_result = %s", coin_flip_result)
-
-        assert coin_flip_result["result"] in ["won", "lost", "tie"]
+        assert coin_flip_result["outcome"][0] in ["won", "lost", "tie"]
     except AssertionError as error:
         # Validate that trying to flip 0 coins causes an error
-        assert isinstance(error, AssertionError) and coin_count == 0
+        assert isinstance(error, AssertionError) and coin_flip_count == 0
